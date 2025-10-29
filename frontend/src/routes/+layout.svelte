477608510e36be56c40a029a7195f2ab8c34fe49
lucabunino <66553738+lucabunino@users.noreply.github.com>
2025-10-29 09:51:11 +0100

    golive

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -114,192 +114,193 @@ function handleKey({key}) {if (key === 'G' && dev) {viewGrid = !viewGrid}}
 	{#if data.seo.SEOTitle}<meta property="og:site_name" content={data.seo.SEOTitle}>{/if}
 </svelte:head>
 
-<header>
-	<!-- Menu -->
-	<nav aria-label="menu" class="menu" class:open={menuOpen} class:up={header.up} class:inverted={header.inverted} bind:clientHeight={headerHeight}>
-		{#if data.info.logo}
-			<a class="logo" href="/" class:active={page.url.pathname === "/"}
-			onclick={() => {menuOpen = false, header.setUp(false), search = undefined}}>
-				<img src={urlFor(data.info.logo)} alt="">
-			</a>
-		{/if}
-		<ul class="jost-74">
-			<li class="menu-item" class:active={page.url.pathname === "/esplora" || page.url.pathname.includes("/esplora/")}>
-				<a href="/esplora"
-				onclick={() => {menuOpen = false, header.setUp(false), search = undefined}}>esplora</a>
-			</li>
-			<li class="menu-item" class:active={page.url.pathname === "/autori" || page.url.pathname.includes("/autori/")}>
-				<a href="/autori"
-				onclick={() => {menuOpen = false, header.setUp(false), search = undefined}}>autori</a>
-			</li>
-			<li class="menu-item" class:active={page.url.pathname === "/live" || page.url.pathname.includes("/live/")}>
-				<a href="/live"
-				onclick={() => {menuOpen = false, header.setUp(false), search = undefined}}>live</a>
-			</li>
-			<li class="menu-item" class:active={page.url.pathname === "/about"}>
-				<a href="/about"
-				onclick={() => {menuOpen = false, header.setUp(false), search = undefined}}
-				>about</a>
-			</li>
-			<li class="menu-item" class:active={page.url.pathname === "/network"}>
-				<a href="/network"
-				onclick={() => {menuOpen = false, header.setUp(false), search = undefined}}>network</a>
-			</li>
-			<li class="menu-item" class:active={page.url.pathname === "/contatti"}>
-				<a href="/contatti"
-				onclick={() => {menuOpen = false, header.setUp(false), search = undefined}}>contatti</a>
-			</li>
-			<form id="search-bar" class="menu-item" onsubmit={(e) => {
-				e.preventDefault()
-				if (search?.trim()) {
-					goto(`/cerca?search=${encodeURIComponent(search)}`)
-					menuOpen = false, header.setUp(false), search = undefined
-				}
-			}}>
-				<input type="text" name="search" id="search" placeholder="cerca" bind:value={search}>
-				<button type="submit" id="search-submit" class="btn bg-gray">
-					cerca
-				</button>
-			</form>
-		</ul>
-		<div id="switchContainer">
-			<div class="desktop-only">
-				{#if searchOpen}
-					<SearchBar bind:searchOpen={searchOpen} search={search} header={true} small={true} mobileAnchorBottom={false} placeholder={"Cerca nel sito"}/>
-				{:else}
-					<button aria-label="Search switch" id="searchSwitch" class="jost-27" onclick={() => searchOpen = !searchOpen}>
-						<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13 13">
-							<path d="M7.96 0c-2.8-.017-5.083 2.25-5.1 5.063-.003 1 .495 2.193 1.043 3.029L0 11.57 1.417 13l3.38-3.888c.808.547 2.087 1.07 3.133 1.074 2.788 0 5.053-2.262 5.07-5.063C13.017 2.31 10.76.017 7.96 0Zm.001 8.861a3.843 3.843 0 0 1-3.864-3.8h-.004A3.859 3.859 0 0 1 7.93 1.207a3.842 3.842 0 0 1 3.838 3.8A3.843 3.843 0 0 1 7.96 8.861h.001Z"/>
-						</svg>
+{#if page.url.hostname === 'framecultura.it' || page.url.hostname === 'www.framecultura.it'}
+	<h1>Manteinance</h1>
+{:else}
+	<header>
+		<nav aria-label="menu" class="menu" class:open={menuOpen} class:up={header.up} class:inverted={header.inverted} bind:clientHeight={headerHeight}>
+			{#if data.info.logo}
+				<a class="logo" href="/" class:active={page.url.pathname === "/"}
+				onclick={() => {menuOpen = false, header.setUp(false), search = undefined}}>
+					<img src={urlFor(data.info.logo)} alt="">
+				</a>
+			{/if}
+			<ul class="jost-74">
+				<li class="menu-item" class:active={page.url.pathname === "/esplora" || page.url.pathname.includes("/esplora/")}>
+					<a href="/esplora"
+					onclick={() => {menuOpen = false, header.setUp(false), search = undefined}}>esplora</a>
+				</li>
+				<li class="menu-item" class:active={page.url.pathname === "/autori" || page.url.pathname.includes("/autori/")}>
+					<a href="/autori"
+					onclick={() => {menuOpen = false, header.setUp(false), search = undefined}}>autori</a>
+				</li>
+				<li class="menu-item" class:active={page.url.pathname === "/live" || page.url.pathname.includes("/live/")}>
+					<a href="/live"
+					onclick={() => {menuOpen = false, header.setUp(false), search = undefined}}>live</a>
+				</li>
+				<li class="menu-item" class:active={page.url.pathname === "/about"}>
+					<a href="/about"
+					onclick={() => {menuOpen = false, header.setUp(false), search = undefined}}
+					>about</a>
+				</li>
+				<li class="menu-item" class:active={page.url.pathname === "/network"}>
+					<a href="/network"
+					onclick={() => {menuOpen = false, header.setUp(false), search = undefined}}>network</a>
+				</li>
+				<li class="menu-item" class:active={page.url.pathname === "/contatti"}>
+					<a href="/contatti"
+					onclick={() => {menuOpen = false, header.setUp(false), search = undefined}}>contatti</a>
+				</li>
+				<form id="search-bar" class="menu-item" onsubmit={(e) => {
+					e.preventDefault()
+					if (search?.trim()) {
+						goto(`/cerca?search=${encodeURIComponent(search)}`)
+						menuOpen = false, header.setUp(false), search = undefined
+					}
+				}}>
+					<input type="text" name="search" id="search" placeholder="cerca" bind:value={search}>
+					<button type="submit" id="search-submit" class="btn bg-gray">
+						cerca
 					</button>
+				</form>
+			</ul>
+			<div id="switchContainer">
+				<div class="desktop-only">
+					{#if searchOpen}
+						<SearchBar bind:searchOpen={searchOpen} search={search} header={true} small={true} mobileAnchorBottom={false} placeholder={"Cerca nel sito"}/>
+					{:else}
+						<button aria-label="Search switch" id="searchSwitch" class="jost-27" onclick={() => searchOpen = !searchOpen}>
+							<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13 13">
+								<path d="M7.96 0c-2.8-.017-5.083 2.25-5.1 5.063-.003 1 .495 2.193 1.043 3.029L0 11.57 1.417 13l3.38-3.888c.808.547 2.087 1.07 3.133 1.074 2.788 0 5.053-2.262 5.07-5.063C13.017 2.31 10.76.017 7.96 0Zm.001 8.861a3.843 3.843 0 0 1-3.864-3.8h-.004A3.859 3.859 0 0 1 7.93 1.207a3.842 3.842 0 0 1 3.838 3.8A3.843 3.843 0 0 1 7.96 8.861h.001Z"/>
+							</svg>
+						</button>
+					{/if}
+				</div>
+				<button aria-label="Menu switch" id="menuSwitch" onclick={(e) => {menuOpen = !menuOpen, header.setUp(false), search = undefined}} class:crossed={menuOpen}>
+					<div class="line"></div>
+					<div class="line"></div>
+					<div class="line"></div>
+				</button>
+			</div>
+		</nav>
+	</header>
+
+	{#key data.pathname + cookieAccepted}
+		<main class:marginTop={!header.initialInverted}>
+			{@render children()}
+		</main>
+	{/key}
+
+	<div class="pre-footer jost-74">
+		<form id="search-bar" class="menu-item" onsubmit={(e) => {
+			e.preventDefault()
+			if (search?.trim()) {
+				goto(`/cerca?search=${encodeURIComponent(search)}`)
+			}
+		}}>
+			<input type="text" name="search" id="search" placeholder={placeholder} bind:value={search}>
+			<button type="submit" id="search-submit" class="btn bg-gray">
+				cerca
+			</button>
+		</form>
+		{#if data.info.newsletter && data.info.newsletterLink}
+			<div class="newsletter mobile-only">
+				<h5>newsletter</h5>
+				<p class="jost-18">{data.info.newsletter}</p>
+				<a class="btn bg-black text-white mobile-w-100" href={data.info.newsletterLink} target="_blank" rel="noopener noreferrer">Iscriviti</a>
+			</div>
+		{/if}
+		{#if data.info.footerPatterns}
+			{@const randomIndex = Math.floor(Math.random() * data.info.footerPatterns.length)}
+			<div class="footer-pattern" style={`background-image: url(${urlFor(data.info.footerPatterns[randomIndex])})`}></div>
+		{/if}
+	</div>
+	<footer class="jost-24 mobile-jost-22" class:marginBottom={data.liveWidget && isPast(data.liveWidget.liveWidget.displayStart)}>
+		<div>
+			{#if data.info.ragioneSociale}<p>{data.info.ragioneSociale}</p>{/if}
+			{#if data.info.adressLabel}
+				{#if data.info.adressGoogleMaps}
+					<a href={data.info.adressGoogleMaps} target="_blank" rel="noopener noreferrer">{data.info.adressLabel}</a>
+				{:else}
+					<p>{data.info.adressLabel}</p>
+				{/if}
+			{/if}
+		</div>
+		<div>
+			{#if data.info.email}<a href="mailto:{data.info.email}">{data.info.email}</a>{/if}
+			{#if data.info.socials}
+				<h4 class="jost-12 uppercase bold">Seguici su</h4>
+				<div class="socials">
+					{#each data.info.socials as social}
+						<a href={social.link} target="_blank" rel="noopener noreferrer"><img class="social" src={urlFor(social.icon)} alt=""></a>
+					{/each}
+				</div>
+			{/if}
+		</div>
+		<nav>
+			<ul>
+				<li class="footer-item" class:active={page.url.pathname === "/esplora" || page.url.pathname.includes("/esplora/")}>
+					<a href="/esplora">esplora</a>
+				</li>
+				<li class="footer-item" class:active={page.url.pathname === "/autori" || page.url.pathname.includes("/autori/")}>
+					<a href="/autori">autori</a>
+				</li>
+				<li class="footer-item" class:active={page.url.pathname === "/live" || page.url.pathname.includes("/live/")}>
+					<a href="/live">live</a>
+				</li>
+				<li class="footer-item" class:active={page.url.pathname === "/about"}>
+					<a href="/about">about</a>
+				</li>
+				<li class="footer-item" class:active={page.url.pathname === "/network"}>
+					<a href="/network">network</a>
+				</li>
+				<li class="footer-item" class:active={page.url.pathname === "/contatti"}>
+					<a href="/contatti">contatti</a>
+				</li>
+			</ul>
+		</nav>
+		{#if data.info.newsletter && data.info.newsletterLink}
+			<div class="desktop-only">
+				<h5>newsletter</h5>
+				<p class="jost-18">{data.info.newsletter}</p>
+				<a class="btn" href={data.info.newsletterLink} target="_blank" rel="noopener noreferrer">Iscriviti</a>
+			</div>
+		{:else}
+			<div class="desktop-only"></div>
+		{/if}
+		<div class="jost-15 mobile-jost-12">
+			<div>
+				<p>©{new Date().getFullYear()} {#if data.info.ragioneSociale}{data.info.ragioneSociale}{/if}</p>
+				{#if data.info.pIva || data.info.ccia}
+					<p>{#if data.info.pIva}P.IVA {data.info.pIva}{/if}</p>
+				{/if}
+				{#if data.policies}
+					{#each data.policies as policy}
+						<!-- <a href="/{policy.kind}">{policy.kind.charAt(0).toUpperCase() + policy.kind.slice(1)}</a> -->
+						<a href="/{policy.kind}">{policy.kind}</a>
+					{/each}
 				{/if}
 			</div>
-			<button aria-label="Menu switch" id="menuSwitch" onclick={(e) => {menuOpen = !menuOpen, header.setUp(false), search = undefined}} class:crossed={menuOpen}>
-				<div class="line"></div>
-				<div class="line"></div>
-				<div class="line"></div>
-			</button>
+			<p>sviluppo: <a class="underline" href="http://www.lucabunino.com" target="_blank" rel="noopener noreferrer">Luca Bunino</a></p>
 		</div>
-	</nav>
-</header>
+	</footer>
 
-<!-- Main -->
-{#key data.pathname + cookieAccepted}
-	<main class:marginTop={!header.initialInverted}>
-		{@render children()}
-	</main>
-{/key}
-
-
-<div class="pre-footer jost-74">
-	<form id="search-bar" class="menu-item" onsubmit={(e) => {
-		e.preventDefault()
-		if (search?.trim()) {
-			goto(`/cerca?search=${encodeURIComponent(search)}`)
-		}
-	}}>
-		<input type="text" name="search" id="search" placeholder={placeholder} bind:value={search}>
-		<button type="submit" id="search-submit" class="btn bg-gray">
-			cerca
-		</button>
-	</form>
-	{#if data.info.newsletter && data.info.newsletterLink}
-		<div class="newsletter mobile-only">
-			<h5>newsletter</h5>
-			<p class="jost-18">{data.info.newsletter}</p>
-			<a class="btn bg-black text-white mobile-w-100" href={data.info.newsletterLink} target="_blank" rel="noopener noreferrer">Iscriviti</a>
-		</div>
-	{/if}
-	{#if data.info.footerPatterns}
-		{@const randomIndex = Math.floor(Math.random() * data.info.footerPatterns.length)}
-		<div class="footer-pattern" style={`background-image: url(${urlFor(data.info.footerPatterns[randomIndex])})`}></div>
-	{/if}
-</div>
-<footer class="jost-24 mobile-jost-22" class:marginBottom={data.liveWidget && isPast(data.liveWidget.liveWidget.displayStart)}>
-	<div>
-		{#if data.info.ragioneSociale}<p>{data.info.ragioneSociale}</p>{/if}
-		{#if data.info.adressLabel}
-			{#if data.info.adressGoogleMaps}
-				<a href={data.info.adressGoogleMaps} target="_blank" rel="noopener noreferrer">{data.info.adressLabel}</a>
-			{:else}
-				<p>{data.info.adressLabel}</p>
-			{/if}
-		{/if}
+	<div id="loader">
+		<ProgressBar color="var(--blue)" zIndex={100}/>
 	</div>
-	<div>
-		{#if data.info.email}<a href="mailto:{data.info.email}">{data.info.email}</a>{/if}
-		{#if data.info.socials}
-			<h4 class="jost-12 uppercase bold">Seguici su</h4>
-			<div class="socials">
-				{#each data.info.socials as social}
-					<a href={social.link} target="_blank" rel="noopener noreferrer"><img class="social" src={urlFor(social.icon)} alt=""></a>
-				{/each}
+
+	{#if !page.url.pathname.includes("/live/streaming/") && data.liveWidget && isPast(data.liveWidget.liveWidget.displayStart)}
+		<Live live={data.liveWidget.liveWidget} />
+	{/if}
+
+	{#if showBanner}
+		<div id="cookie-banner" class="jost-15 shadow rounded">
+			<p>Questo sito utilizza servizi di terze parti come Youtube che possono impostare cookie. Avviando la riproduzione di un video, consenti l’uso dei relativi cookie.{#each data.policies as policy}{#if policy.kind == 'cookies'}{@html ' '}Per saperne di più, consulta la nostra <a href="/cookies" class="underline">cookie policy</a>{/if}{/each}</p>
+			<div id="cookie-btns">
+				<button id="accept-cookies" onclick={acceptCookies} class="jost-12 bold uppercase">Ok, ho capito</button>
 			</div>
-		{/if}
-	</div>
-	<nav>
-		<ul>
-			<li class="footer-item" class:active={page.url.pathname === "/esplora" || page.url.pathname.includes("/esplora/")}>
-				<a href="/esplora">esplora</a>
-			</li>
-			<li class="footer-item" class:active={page.url.pathname === "/autori" || page.url.pathname.includes("/autori/")}>
-				<a href="/autori">autori</a>
-			</li>
-			<li class="footer-item" class:active={page.url.pathname === "/live" || page.url.pathname.includes("/live/")}>
-				<a href="/live">live</a>
-			</li>
-			<li class="footer-item" class:active={page.url.pathname === "/about"}>
-				<a href="/about">about</a>
-			</li>
-			<li class="footer-item" class:active={page.url.pathname === "/network"}>
-				<a href="/network">network</a>
-			</li>
-			<li class="footer-item" class:active={page.url.pathname === "/contatti"}>
-				<a href="/contatti">contatti</a>
-			</li>
-		</ul>
-	</nav>
-	{#if data.info.newsletter && data.info.newsletterLink}
-		<div class="desktop-only">
-			<h5>newsletter</h5>
-			<p class="jost-18">{data.info.newsletter}</p>
-			<a class="btn" href={data.info.newsletterLink} target="_blank" rel="noopener noreferrer">Iscriviti</a>
 		</div>
-	{:else}
-		<div class="desktop-only"></div>
 	{/if}
-	<div class="jost-15 mobile-jost-12">
-		<div>
-			<p>©{new Date().getFullYear()} {#if data.info.ragioneSociale}{data.info.ragioneSociale}{/if}</p>
-			{#if data.info.pIva || data.info.ccia}
-				<p>{#if data.info.pIva}P.IVA {data.info.pIva}{/if}</p>
-			{/if}
-			{#if data.policies}
-				{#each data.policies as policy}
-					<!-- <a href="/{policy.kind}">{policy.kind.charAt(0).toUpperCase() + policy.kind.slice(1)}</a> -->
-					<a href="/{policy.kind}">{policy.kind}</a>
-				{/each}
-			{/if}
-		</div>
-		<p>sviluppo: <a class="underline" href="http://www.lucabunino.com" target="_blank" rel="noopener noreferrer">Luca Bunino</a></p>
-	</div>
-</footer>
-
-<div id="loader">
-	<ProgressBar color="var(--blue)" zIndex={100}/>
-</div>
-
-{#if !page.url.pathname.includes("/live/streaming/") && data.liveWidget && isPast(data.liveWidget.liveWidget.displayStart)}
-	<Live live={data.liveWidget.liveWidget} />
-{/if}
-
-{#if showBanner}
-	<div id="cookie-banner" class="jost-15 shadow rounded">
-		<p>Questo sito utilizza servizi di terze parti come Youtube che possono impostare cookie. Avviando la riproduzione di un video, consenti l’uso dei relativi cookie.{#each data.policies as policy}{#if policy.kind == 'cookies'}{@html ' '}Per saperne di più, consulta la nostra <a href="/cookies" class="underline">cookie policy</a>{/if}{/each}</p>
-		<div id="cookie-btns">
-			<button id="accept-cookies" onclick={acceptCookies} class="jost-12 bold uppercase">Ok, ho capito</button>
-		</div>
-	</div>
 {/if}
 
 <style>
